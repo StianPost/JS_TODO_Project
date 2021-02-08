b10c8211ed89a3125e46aff1ac43954523ce4b69
StianPost <69841774+StianPost@users.noreply.github.com>
2021-02-08 21:48:41 +0100

    added validator and error message when typing in empty text

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const addBtn = document.querySelector('#addItem');
 const removeBtn = document.querySelector('#removeItem');
 const todoItem = document.querySelector('#todoItem');
+const error = document.querySelector('.errorDiv');
 let count = 0;
 
 addBtn.onclick = function (event) {
@@ -10,12 +11,28 @@ addBtn.onclick = function (event) {
   if (count % 2 === 0) {
     itemClass = 'even';
   }
-  document.querySelector('#itemsList').innerHTML += `
+  if (!todoItem.value.trim()) {
+    console.log('ERROR');
+    error.innerHTML = `
+    <div class="errorMSG"
+    <h2>Please write valid input! </h2>
+    </div>
+    `;
+    error.style.display = 'block';
+    count--;
+    setTimeout(function () {
+      error.innerHTML = ``;
+      error.style.display = 'none';
+    }, 3000);
+  } else {
+    document.querySelector('#itemsList').innerHTML += `
   <div class="${itemClass}">
   <h2>Number: ${count} </h2>
   <h2>Todo: ${todoItem.value}</h2>
 </div>
   `;
+  }
+
   todoItem.value = '';
 };
 
